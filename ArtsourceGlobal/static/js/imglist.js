56ff788056ebeee4ea832c64fbfc06463e53dbd2
u6743850 <zongyuan.sui@anu.edu.au>
2020-04-29 19:44:16 +1000

    the auto-fit images

--- ArtsourceGlobal/static/js/imglist.js
+++ ArtsourceGlobal/static/js/imglist.js
@@ -1,20 +1,20 @@
 
 function TwoImagesOneLine() {
-	var images=document.getElementsByClassName("image");
+	var images=document.getElementsByClassName("imgItem");
 	for(var i=0;i<images.length;i++) {
     images[i].id = 'image2';
 	}
 };
 
 function ThreeImagesOneLine() {
-	var images=document.getElementsByClassName("image");
+	var images=document.getElementsByClassName("imgItem");
 	for(var i=0;i<images.length;i++) {
     images[i].id = 'image3';
 	}
 };
 
 function FourImagesOneLine() {
-	var images=document.getElementsByClassName("image");
+	var images=document.getElementsByClassName("imgItem");
 	for(var i=0;i<images.length;i++) {
     images[i].id = 'image4';
 	}
