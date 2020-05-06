73c684556400375ef150d82b475c78acdcae1faf
u6743850 <zongyuan.sui@anu.edu.au>
2020-05-06 15:51:55 +1000

    finish the thrid party authentication

--- ArtsourceGlobal/oauth/urls.py
+++ ArtsourceGlobal/oauth/urls.py
@@ -6,8 +6,8 @@ from oauth import views
 urlpatterns = [
     path('instagram/', views.instagram),
     path('instagram_callback/', views.instagram_callback),
-    path('facebook/', views.facebook),
-    path('facebook_callback/', views.facebook_callback),
+    # path('facebook/', views.facebook),
+    # path('facebook_callback/', views.facebook_callback),
     # path('github/', views.github),
     # path('github_callback/', views.github_callback),
     # url(r'^active/(?P<active_code>.*)/$', views.ActiveUserView.as_view(), name="user_active"),
--- ArtsourceGlobal/oauth/views.py
+++ ArtsourceGlobal/oauth/views.py
@@ -13,7 +13,6 @@ from user.form import RegisterForm
 import requests
 
 
-# TODO: set up the https to test
 def instagram(request):
     # The below client id and secret should be changed after you get your own id and secret
     redirect_uri = 'https://localhost:8000/oauth/instagram_callback'
@@ -24,63 +23,55 @@ def instagram(request):
 
 
 def instagram_callback(request):
-    message = request.get_full_path()
-    print("in the callback")
-    print("")
-    print("!!!!!!!!!!!1")
-    print("")
-    print("end line")
-
-    code = message.split("code=")[1].split("#_")[0]
-    url = "https://api.instagram.com/oauth/access_token"
+    code = request.get_full_path().split("code=")[1].split("#_")[0]
+    url = "https://api.instagram.com/oauth/access_token/"
     data = {"client_id": "2861822790566791",
             "client_secret": "9f6c11ccc2a1076130eaea5104fc7303",
             "grant_type": "authorization_code",
             "redirect_uri": "https://localhost:8000/oauth/instagram_callback",
             "code": code
-            },
-    r = requests.post(url, data=json.dumps(data))
-    print(r)
+            }
+    r = requests.post(url, data=data)
     # return redirect(url)
     json_data = json.loads(r.text)
     access_token = json_data.get("access_token")
-    print(access_token)
-    user_id = json_data.get("user_id")
-    response = requests.get("https://graph.instagram.com/"+user_id+"?fields=username&access_token="+access_token)
+    user_id = str(json_data.get("user_id"))
+    response = requests.get("https://graph.instagram.com/"+user_id+"?fields=id,username&access_token="+access_token)
     username = json.loads(response.text).get("username")
-    message = username
+    message = "please create a local account!"
     register_form = RegisterForm(initial={
         'username': username,
     })
     return render(request, "user/register.html", {'message': message, 'register_form': register_form})
 
 
-def facebook(request):
-    # The below client id and secret should be changed after you get your own id and secret
-    redirect_uri = 'https://localhost:8000/oauth/facebook_callback'
-    url = "https://www.facebook.com/v6.0/dialog/oauth?client_id=1052296011819502" \
-          "&redirect_uri=" + redirect_uri + "&state={\"st=state123abc,ds=123456789}\""
-
-    return redirect(url)
-
-
-def facebook_callback(request):
-    redirect_uri = 'https://localhost:8000/oauth/facebook_callback'
-    print("in the facebook callback")
-    message = request.get_full_path()
-    print(message)
-    print("this is the body")
-    print(request.body)
-    if message.find("code=") != -1:
-        code = message.split("code=")[1].split("&state=")[0]
-        url = "https://graph.facebook.com/v6.0/oauth/access_token?client_id=1052296011819502&" \
-              "&redirect_uri=" + redirect_uri + "&client_secret=fcb79bc8754e158ea251428f90b96248" + \
-              "&code=" + code
-        return redirect(url)
-
-    # resp = oauth.github.get(url='https://api.github.com/user', token=token)
-    #     profile = resp.json()
-    return render(request, "oauth/index.html", {'message': message})
+#
+# def facebook(request):
+#     # The below client id and secret should be changed after you get your own id and secret
+#     redirect_uri = 'https://localhost:8000/oauth/facebook_callback'
+#     url = "https://www.facebook.com/v6.0/dialog/oauth?client_id=1052296011819502" \
+#           "&redirect_uri=" + redirect_uri + "&state={\"st=state123abc,ds=123456789}\""
+#
+#     return redirect(url)
+#
+#
+# def facebook_callback(request):
+#     redirect_uri = 'https://localhost:8000/oauth/facebook_callback'
+#     print("in the facebook callback")
+#     message = request.get_full_path()
+#     print(message)
+#     print("this is the body")
+#     print(request.body)
+#     if message.find("code=") != -1:
+#         code = message.split("code=")[1].split("&state=")[0]
+#         url = "https://graph.facebook.com/v6.0/oauth/access_token?client_id=1052296011819502&" \
+#               "&redirect_uri=" + redirect_uri + "&client_secret=fcb79bc8754e158ea251428f90b96248" + \
+#               "&code=" + code
+#         return redirect(url)
+#
+#     # resp = oauth.github.get(url='https://api.github.com/user', token=token)
+#     #     profile = resp.json()
+#     return render(request, "oauth/index.html", {'message': message})
 
 
 #
