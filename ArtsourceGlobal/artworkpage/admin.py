34105b166e08791876dfdc49f814e97f5ae80326
u6743850 <zongyuan.sui@anu.edu.au>
2020-05-09 18:07:19 +1000

    add to admin site

--- ArtsourceGlobal/artworkpage/admin.py
+++ ArtsourceGlobal/artworkpage/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+admin.site.register(models.ArtistNames)
+admin.site.register(models.TagsNames)
+admin.site.register(models.Artwork)
+admin.site.register(models.Category)
--- ArtsourceGlobal/homepage/admin.py
+++ ArtsourceGlobal/homepage/admin.py
@@ -1,6 +1,3 @@
 from django.contrib import admin
 from .models import Artwork, Business, Booking
 # Register your models here.
-admin.site.register(Artwork)
-admin.site.register(Business)
-admin.site.register(Booking)
