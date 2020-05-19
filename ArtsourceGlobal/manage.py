61ef68f1d9d74360c65e247b147e1d1d0dc5dead
Yu Qiao <u6706067@anu.edu.au>
2020-05-20 04:28:43 +0800

    mark

--- ArtsourceGlobal/manage.py
+++ ArtsourceGlobal/manage.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
-        ) from exc
+        )(exc)
     execute_from_command_line(sys.argv)
 
     # execute_from_command_line(['manage.py', 'runserver', '127.0.0.1:8000'])
